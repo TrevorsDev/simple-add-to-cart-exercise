e07d3e3d78cd14293c34584f57165a8dfad84f46
Trevor Johnson <trevor9j@protonmail.com>
2024-04-09 13:56:08 -0700

    practice probs in arr&loops, added delete buttons and date to todoList

--- scripts/08-todo-list.js
+++ scripts/08-todo-list.js
@@ -1,18 +1,36 @@
-const todoList = ['make dinner', 'wash dishes'];
+-const todoList = [{
+  name: 'make dinner',
+  dueDate: '2024-04-09'
+}, {
+  name: 'wash dishes',
+  dueDate: '2024-04-09'
+}];
 
-renderTodoList(); 
+renderTodoList();
 
 function renderTodoList() {
   let todoListHTML = '';
 
-for (let i = 0; i < todoList.length; i++) {
-  const todo = todoList[i];
-  const html = `<p>${todo}</p>`;
-  todoListHTML += html;
-}
-console.log(todoListHTML);
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    // lines 17 and 18 are the long hand version of line 19
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
 
-document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+    const html = `
+    <p>
+      ${name} ${dueDate}
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      ">Delete</button>
+    </p>
+  `;
+    todoListHTML += html;
+  }
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 
 
@@ -20,8 +38,16 @@ function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const date = dateInputElement.value;
+
+  todoList.push({
+    // Lines 47 and 48 do the same as lines 49 and 50
+    // name: name,
+    // dueDate: dueDate,
+    name,
+    dueDate
+});
 
   inputElement.value = '';
 
